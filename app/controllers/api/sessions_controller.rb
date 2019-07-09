979b2e7aa8dc32b4b933348d4811a3b71fbd379a
Calvin Nguyen <calvindnguyen714@gmail.com>
2019-07-09 10:39:16 -0700

    Revert "Add authenticity tokens back"
    This reverts commit b66095d7b40942c697a8cbc9c179a69c6a71db66.
    
    Undoing source model changes

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Api::SessionsController < ApplicationController
-  # skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   before_action :require_logged_in, only: [:destroy]
 
