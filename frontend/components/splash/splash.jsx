5dd11900dc8f4bc64dc77f6602b4e34f729c8dda
Calvin Nguyen <calvindnguyen714@gmail.com>
2019-08-15 17:33:50 -0700

    Skeleton splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -7,10 +7,16 @@ class Splash extends React.Component {
   render() {
     return (
       <div id="title-splash">
-        <h1>Where readers become leaders</h1>
-        <h2>Keep up with all the topics that matter to you. <br />
-          All in one place.
-        </h2>
+        <div className="splash-wrapper">
+          <section className="splash-header">
+            <h1>Where readers become leaders</h1>
+            <h2>
+              Keep up with all the topics that matter to you. <br />
+              All in one place.
+          </h2>
+          </section>
+
+        </div>
       </div>
     );
   }
