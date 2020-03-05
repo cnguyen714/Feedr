3cdde9ecc40c05605557ef593617b73f80f44bbc
Calvin Nguyen <calvindnguyen714@gmail.com>
2020-03-04 19:58:36 -0800

    Add scrolling and footer to splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -18,6 +18,33 @@ class Splash extends React.Component {
 
           <img className="splash-img" />
         </div>
+
+        <footer id="footer">
+          Keep in touch
+          <ul className="icons">
+            <li><a
+              href="https://github.com/cnguyen714">
+              <i className="icon fab fa-github"></i>
+              </a></li>
+            <li><a
+              href="https://www.linkedin.com/in/cdnguyen714/">
+              <i className="icon fab fa-linkedin-in"></i>
+              </a></li>
+            <li><a
+              href="https://angel.co/calvin-nguyen-2">
+              <i className="icon fab fa-angellist"></i>
+              </a></li>
+            <li><a 
+              href="mailto:calvindnguyen714@gmail.com">
+              <i className="icon fas fa-envelope"></i>
+              </a></li>
+            <li><a 
+              href="https://docs.google.com/document/d/1PDJEhGBPwgXqI44NVkrDE0Oyb1MkzHG6TUREMHM_-Mo/edit?usp=sharing">
+              <i className="icon fas fa-file"></i>
+              </a>
+            </li>
+          </ul>
+        </footer>
       </div>
     );
   }
